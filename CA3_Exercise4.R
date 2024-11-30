f7e17dd304a20d849fcec29ccf1c48008625434c
Roisin1m <roisinmurphy2015@gmail.com>
2024-11-30 21:35:22 +0000

    extract the estimated coefficients β hat and the estimated residual standard deviation parameter σ hat from a fitted object using lm()

---
+++ CA3_Exercise4.R
@@ -0,0 +1,89 @@
+## Question 3
+### Part 1
+
+
+df <- with(mtcars, data.frame(y=mpg, x1=disp, x2=hp, x3=wt))
+
+
+### Part 2
+nll_lm <- function(data, par) {
+  y <- data$y
+  X <- as.matrix(cbind(1, data[, c("x1", "x2", "x3")]))
+  beta <- par[1:(ncol(X))]
+  sigma <- par[length(par)]
+  
+  res <- y - X %*% beta
+  
+  nll <- - sum(dnorm(res, mean = 0, sd = sigma, log = TRUE))
+  
+  return(nll)
+}
+
+
+### Part 3
+
+initial_beta <- c(mean(df$y), rep(0, ncol(df) - 2)) 
+initial_sigma <- sd(df$y) 
+initial_par <- c(initial_beta, initial_sigma)
+
+result <- optim(par = initial_par, fn = nll_lm, data = df, method = "L-BFGS-B", 
+                lower = c(rep(-Inf, length(initial_beta)), 0.000001), 
+                upper = c(rep(Inf, length(initial_beta)), Inf))
+
+beta_hat <- result$par[1:(ncol(df))]
+sigma_hat <- result$par[length(result$par)]
+
+print(beta_hat)
+print(sigma_hat)
+
+
+### Part 4
+#It is necessary to implement the negative log-likelihood function because "optim()" minimizes function by default. Minimizing the negative log likelihood is the same as maximizing the log likelihood, which is the same as maximizing the likelihood. This transformation does not change the parameter estimates, they stay the same. 
+
+### Part 5
+
+X <- as.matrix(cbind(1, df[, c("x1", "x2", "x3")]))
+y <- df$y
+
+B_matrix <- solve(t(X) %*% X) %*% t(X) %*% y
+B_matrix
+
+beta_hat_optim <- result$par[1:(ncol(X))]
+beta_hat_optim
+
+
+### Part 6
+
+res<- y- X %*% B_matrix
+
+S_matrix <- sqrt(sum(res^2) / length(y)- ncol(X))
+
+
+sigma_hat
+
+S_matrix
+
+### Part 7
+
+
+
+### Part 8
+
+
+hessian_result <- optim(par = initial_par, fn = nll_lm, data = df, method = "L-BFGS-B", 
+                        lower = c(rep(-Inf, length(initial_beta)), 0.00001), 
+                        upper = c(rep(Inf, length(initial_beta)), Inf), hessian = TRUE)
+
+hessian <- hessian_result$hessian
+standard_error <- sqrt(diag(solve(hessian)))
+
+standard_error
+
+## Git
+lm_fit <- lm(mpg ~ disp + hp + wt, data = mtcars)
+
+beta_hat1 <- coef(lm_fit)
+beta_hat1
+
+sigma_hat1 <- summary(lm_fit)$sigma
+sigma_hat1
